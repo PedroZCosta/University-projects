f450d43da219343a27180d1725e54dc4ce5e88f5
Pedro Costa <161760181+PedroZCosta@users.noreply.github.com>
2024-04-02 21:33:32 -0300

    exec.py

--- exec.py
+++ exec.py
@@ -1,19 +1,30 @@
-#Exercício 2: Calculadora de IMC (Índice de Massa Corporal)
-peso = float(input("Qual seu peso? "))
-alt = float(input("Qual sua altura? (Exemplo : 1.83) "))
-imc = int(peso / (alt **2))
-print ("Seu resultado e:", imc)
-if imc <= 16.9:
-    print("Voce esta muito abaixo do peso!")
-elif (imc >= 17) and (imc <= 18.9):
-    [print ("Voce esta abaixo do peso!")]
-elif (imc >= 18.5) and (imc <= 24.9):
-    [print ("Voce tem um peso normal!")]
-elif (imc >= 25) and (imc <= 29.9):
-    [print ("Voce esta acima do peso!")]
-elif (imc >= 30) and (imc <= 34.9):
-    [print("Voce tem obesidade grau 1!")]
-elif (imc >= 35) and (imc <= 40):
-    [print ("Voce tem obesidade grau 2!")]
+"""
+Exercicio: IMC – Índice de Massa Corporal
+Criador: Pedro Henrique Zakrzevski Costa
+Data: 29/03/2024
+versao: 1.4
+"""
+# Faixas do IMC
+range1 = 16.5
+range2 = 24.9
+range3 = 29.9
+range4 = 34.9
+range5 = 39.9
+# Solicita as informacoes do usuario
+peso = float(input("Digite seu peso? "))
+alt = float(input("Digite sua altura em metros (exemplo : 1.83): "))
+imc = (peso / (alt ** 2))
+print(f"Seu resultado é:{imc: .1f}")
+# Encontra onde o usuario se encontra na tabela do IMC
+if imc < range1:
+    print("Voce esta abaixo do peso!")
+elif (imc >= range1) and (imc < range2):
+    print("Voce tem um peso normal!")
+elif (imc >= range2) and (imc < range3):
+    print("Voce esta acima do peso!")
+elif (imc >= range3) and (imc < range4):
+    print("Voce tem obesidade grau 1!")
+elif imc >= range4 and (imc < range5):
+    print("Voce tem obesidade grau 2!")
 else:
-    print ("Voce tem obesidade grau 3!")
+    print("Voce tem obesidade grau 3!")
